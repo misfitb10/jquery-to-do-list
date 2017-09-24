ce484e4bc5b53e2e0f813f571acfa5dc2d54c546
Bilal Hussain <bilal.hussain@incentro.com>
2017-09-24 15:40:05 +0200

    Refactored some stuff, more functions, more animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,121 @@
-function addListItem() {
+function ToDoList() {
+    // Create container for the list
+    createListContainer();
+
+    var $inputStuff = $('.input--stuff');
+
+    // Add list items through ADD button
     var $submitStuff = $('.submit--stuff');
-    var $inputElement = $('.input--stuff');
+    if ($submitStuff.length) {
+        $submitStuff.on('click', addListItem);
+    }
+
+    // Add list items through ENTER
+    if ($inputStuff.length) {
+        $inputStuff.bind('keypress', function(e) {
+            var codeEnter = 13;
+
+            if (e.keyCode === codeEnter) {
+                addListItem();
+            }
+        });
+    }
+
+    // Check if the input field is empty
+    if ($inputStuff.length) {
+        $inputStuff.on('input', checkEmptyValue);
+    }
+
+    // Remove entire list
+    var $btnRemoveEntireList = $('.btn--removeEntireList');
+    if ($btnRemoveEntireList) {
+        $btnRemoveEntireList.on('click', removeEntireList);
+    }
+}
+
+function createListContainer() {
+    var $listContainer = $('.listContainer');
     var list = '<ul class="todoList"></ul>';
+
+    // Append unordered list to the list container, because we don't want empty ul's and li's in the DOM
+    return $listContainer.append(list);
+}
+
+function createListItem() {
+    var $inputElement = $('.input--stuff');
+    return '<li>' + $inputElement.val() + ' <a href="#" class="btn btn--removeListItem">X</a></li>'
+}
+
+function createRemoveEntireListButton() {
+    return '<a href="#" class="btn btn--removeEntireList">Remove entire list</a>'
+}
+
+function addListItem(e) {
+    if (e) { e.preventDefault() }
+
+    var $inputElement = $('.input--stuff');
+    var $btnRemoveEntireList = $('.btn--removeEntireList');
     var $errorMessage = $('.message--error');
+    var $todoList = $('.todoList');
     var fadeSpeed = 245;
 
-    // Append unordered list to the list container, because we don't want empty ul's and li's in the DOM.
-    $('.listContainer').append(list);
+    // Check if the value is not empty
+    if ($inputElement.val() !== '') {
+        $todoList.append(createListItem());
 
-    // Filter button
-    $submitStuff.on('click', function(e) {
-        e.preventDefault();
+        // Remove list item (bind only once!)
+        $('.btn--removeListItem').off().on("click", function(e){
+            e.preventDefault();
+            removeListItem(this);
+        });
 
-        if ( $inputElement.val() !== '' ) {
-            $inputElement.removeClass('input--error');
-            $('.todoList').append('<li>'+$inputElement.val()+' <a href="#" class="removeListItem">remove</a></li>');
-            removeListItem();
-        } else {
-            $inputElement.addClass('input--error');
-            $errorMessage.fadeIn(fadeSpeed);
+        // If there are more than 1 list items, show the `remove entire list` button
+        if ($('.todoList li').length > 1) {
+            if ( !$btnRemoveEntireList.length ) {
+                $todoList.after(createRemoveEntireListButton);
+
+                // Bind remove entire list
+                $('.btn--removeEntireList').on('click', removeEntireList);
+            }
         }
-    });
-
-    $(".input--stuff").on("input", function(e) {
-        // Check if the value is empty or not
-        if (this.value.length > 0) {
-            $errorMessage.fadeOut(fadeSpeed);
-            $inputElement.removeClass('input--error');
-        } else {
-            $errorMessage.fadeIn(fadeSpeed);
-            $inputElement.addClass('input--error');
-        }
-    });
+    } else {
+        $inputElement.addClass('input--error');
+        $errorMessage.fadeIn(fadeSpeed);
+    }
 }
 
-function removeListItem() {
-    var $removeListItem = $('.removeListItem');
+function checkEmptyValue() {
+    var $errorMessage = $('.message--error');
+    var $inputElement = $('.input--stuff');
+    var errorClass = 'input--error';
+    var fadeSpeed = 245;
 
-    $removeListItem.on('click', function(e) {
-        e.preventDefault();
-        $(this).parent().fadeOut(249);
-    });
+    if ($inputElement.val().length > 0) {
+        $errorMessage.fadeOut(fadeSpeed);
+        $inputElement.removeClass(errorClass);
+    } else {
+        $errorMessage.fadeIn(fadeSpeed);
+        $inputElement.addClass(errorClass);
+    }
 }
 
-function ToDoList() {
-    addListItem();
-    removeListItem();
+function removeListItem(thisItem) {
+    $(thisItem).parent().remove();
+    checkEmptyList();
 }
 
-$(document).ready(function() {
+function removeEntireList(e) {
+    if (e) { e.preventDefault() }
+    $('.btn--removeEntireList, .todoList li').remove();
+}
+
+function checkEmptyList() {
+    var $todoListItem = $('.todoList li');
+    if ($todoListItem.length === 0) {
+        $('.btn--removeEntireList').remove();
+    }
+}
+
+$(function() {
     ToDoList();
 });
